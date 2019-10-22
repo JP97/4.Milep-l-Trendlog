24cc61792eb942e0bde9ecc5305fb7ccbd084761
Julian <juli11c0@edu.eal.dk>
2019-10-22 13:19:44 +0200

    Add project files.

---
+++ 4.Milepæl-Trendlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _4.Milepæl_Trendlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
